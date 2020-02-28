3ced02891d8147f4ad0e212ca71bd80c934d84ed
Joe Chung <s1100841@monmouth.edu>
2020-02-28 12:07:20 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const request = require('request')
+
+const url = 'https://strawpoll.me/api/v2/polls/19458348'
+
+request({url:url,json:true},(err,res)=>{
+    const data = res.body
+    console.log(data.title)
+    for(var i = 0; i < data.options.length; i++) {
+        console.log('Votes for ' + data.options[i] + ': ' + data.votes[i])
+    }
+})
