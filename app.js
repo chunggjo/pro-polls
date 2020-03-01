ae42055efbf23edd4b8d753d26372d60547bb8e4
Joe Chung <s1100841@monmouth.edu>
2020-02-29 19:22:43 -0500

    Adding basic error handling

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const request = require('request')
 
-const url = 'https://strawpoll.me/api/v2/polls/19458348'
+const url = 'https://www.strawpoll.me/api/v2/polls/19458348'
 
 request({url:url,json:true},(err,res)=>{
-    const data = res.body
-    console.log(data.title)
-    for(var i = 0; i < data.options.length; i++) {
-        console.log('Votes for ' + data.options[i] + ': ' + data.votes[i])
+    if(err) {
+        console.log('Unable to connect to service.')
+    } else if(!res.body) {
+        console.log('Could not find that poll ID.')
+    } else {
+        const data = res.body
+        console.log(data.title)
+        for(var i = 0; i < data.options.length; i++) {
+            console.log('Votes for ' + data.options[i] + ': ' + data.votes[i])
+        }
     }
 })
