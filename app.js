57d4160d5c8cbeaf3334539f0a76357a945fd50e
Joe Chung <s1100841@monmouth.edu>
2020-03-01 18:13:32 -0500

    Adding user input for getPoll

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const request = require('request')
 const getPoll = require('./utils/getPoll')
 
-getPoll('19458348',(err,data)=>{
-    console.log('Error',err)
-    console.log('Data',data)
-    // console.log(data.title)
-    // for(var i = 0; i < data.options.length; i++) {
-    //     console.log('Votes for ' + data.options[i] + ': ' + data.votes[i])
-    // }
+const id = process.argv[2]
+if(!id){
+    return console.log('Please provide a poll ID')
+}
+// 19458348
+getPoll(id,(err,data)=>{
+    if(err){
+        return console.log(err)
+    }
+    console.log(data.title)
+    for(var i = 0; i < data.options.length; i++) {
+        console.log('Votes for ' + data.options[i] + ': ' + data.votes[i])
+    }
 })
