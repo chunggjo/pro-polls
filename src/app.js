e124f48addd5c55aaebb39cecb69d9b6f24a803c
Joe Chung <s1100841@monmouth.edu>
2020-03-02 18:11:04 -0500

    Adding hbs

--- src/app.js
+++ src/app.js
@@ -6,10 +6,13 @@ const express = require('express')
 const app = express()
 const publicDirectoryPath = path.join(__dirname,'../public')
 
+app.set('view engine','hbs')
 app.use(express.static(publicDirectoryPath))
 
 app.get('',(req,res)=>{
-    
+    res.render('index',{
+        greeting:'Welcome to AnonVote!'
+    })
 })
 
 app.listen(3000, ()=>{
