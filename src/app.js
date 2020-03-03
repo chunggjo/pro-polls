2375f84c22f8dbffa4a36961aaacc9aef1e14beb
Joe Chung <s1100841@monmouth.edu>
2020-03-02 19:10:44 -0500

    Customizing again views directory

--- src/app.js
+++ src/app.js
@@ -4,11 +4,17 @@ const getPoll = require('../utils/getPoll')
 const express = require('express')
 
 const app = express()
-const publicDirectoryPath = path.join(__dirname,'../public')
-const viewsPath = path.join(__dirname,'../templates')
 
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname,'../public')
+const viewsPath = path.join(__dirname,'../templates/views')
+const partialsPath = path.join(__dirname,'../templates/partials')
+
+// Setup handlebars engine and views location
 app.set('view engine','hbs')
 app.set('views',viewsPath)
+
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
 app.get('',(req,res)=>{
