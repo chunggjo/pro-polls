06c49a1a6383d5e28f002d1c84c78a494938d2fa
Joe Chung <s1100841@monmouth.edu>
2020-03-04 12:38:32 -0500

    Adding search form

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,19 @@
+const pollIdForm = document.querySelector('form')
+const search = document.querySelector('input')
+pollIdForm.addEventListener('submit',(e)=>{
+    e.preventDefault()
 
-fetch('http://localhost:3000/vote?id=19458348').then((res)=>{
-    res.json().then((data)=>{
-        if(data.error){
-            console.log(data.error)
-        } else {
-            console.log(data.pollName)
-            for(var i = 0; i < data.options.length; i++) {
-                console.log('Votes for ' + data.options[i] + ': ' + data.votes[i])
+    const id = search.value
+    fetch('http://localhost:3000/vote?id='+id).then((res)=>{
+        res.json().then((data)=>{
+            if(data.error){
+                console.log(data.error)
+            } else {
+                console.log(data.pollName)
+                for(var i = 0; i < data.options.length; i++) {
+                    console.log('Votes for ' + data.options[i] + ': ' + data.votes[i])
+                }
             }
-        }
+        })
     })
 })
