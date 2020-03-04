37b7a1dcddd8e890aa9fabcc6bde34c64f6c2235
Joe Chung <s1100841@monmouth.edu>
2020-03-04 14:00:41 -0500

    Search poll by ID

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,27 @@
 const pollIdForm = document.querySelector('form')
 const search = document.querySelector('input')
+const pollName = document.querySelector('#pollName')
+const message = document.querySelector('#message')
+
 pollIdForm.addEventListener('submit',(e)=>{
     e.preventDefault()
 
     const id = search.value
+
+    message.pollName = ''
+    message.textContent = 'Loading...'
+    $('#votes').empty()
     fetch('http://localhost:3000/vote?id='+id).then((res)=>{
         res.json().then((data)=>{
             if(data.error){
-                console.log(data.error)
+                message.textContent=data.error
             } else {
-                console.log(data.pollName)
+                pollName.textContent=data.pollName
+                message.textContent=''
                 for(var i = 0; i < data.options.length; i++) {
-                    console.log('Votes for ' + data.options[i] + ': ' + data.votes[i])
+                    $('#votes').append('<li>'+data.options[i]+' - '+data.votes[i]+' votes</li>')
                 }
+                
             }
         })
     })
