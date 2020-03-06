60abb05564d8bdbc8d9da17aed6daa0a216bf3d1
Christopher Calva <christopher.calva@gmail.com>
2020-03-06 11:15:26 -0500

    added poll creation page

--- src/app.js
+++ src/app.js
@@ -7,49 +7,53 @@ const hbs = require('hbs')
 const app = express()
 
 // Define paths for Express config
-const publicDirectoryPath = path.join(__dirname,'../public')
-const viewsPath = path.join(__dirname,'../templates/views')
-const partialsPath = path.join(__dirname,'../templates/partials')
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 // Setup handlebars engine and views location
-app.set('view engine','hbs')
-app.set('views',viewsPath)
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
 hbs.registerPartials(partialsPath)
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
-app.get('',(req,res)=>{
-    res.render('index',{
-        title:'Welcome to AnonVote!'
-    })
+app.get('', (req, res) => {
+	res.render('index', {
+		title: 'Welcome to AnonVote!'
+	})
 })
-app.get('/vote',(req,res)=>{
-
-    if(!req.query.id){
-        return res.send({error:'Must provide a poll ID.'})
-    }
-    getPoll(req.query.id,(error,{title,options,votes,multi}={})=>{
-        if(error){
-            return res.send({error})
-        }
-        return res.send({
-            pollName:title,
-            options,
-            votes,
-            multi
-        })
-    })
-})
-
-app.get('*',(req,res)=>{
-    res.render('404',{
-        title:'404',
-        errorMessage:'Page not found'
-    })
+app.get('/vote', (req, res) => {
+	if (!req.query.id) {
+		return res.send({ error: 'Must provide a poll ID.' })
+	}
+	getPoll(req.query.id, (error, { title, options, votes, multi } = {}) => {
+		if (error) {
+			return res.send({ error })
+		}
+		return res.send({
+			pollName: title,
+			options,
+			votes,
+			multi
+		})
+	})
 })
 
-app.listen(3000, ()=>{
-    console.log('Server up on port 3000')
+app.get('/create', (req, res) => {
+	res.render('create', {
+		title: 'Create your poll'
+	})
 })
 
+app.get('*', (req, res) => {
+	res.render('404', {
+		title: '404',
+		errorMessage: 'Page not found'
+	})
+})
+
+app.listen(3000, () => {
+	console.log('Server up on port 3000')
+})
