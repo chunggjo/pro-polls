4a5742a2f2e891cebd071ec7baa01d2a817e0d4a
Joe Chung <s1100841@monmouth.edu>
2020-03-07 18:38:45 -0500

    updating index view and client side js

--- public/js/client.js
+++ public/js/client.js
@@ -38,13 +38,17 @@ pollIdForm.addEventListener('submit',(e)=>{
 // Vote on poll
 voteForm.addEventListener('submit',(e)=>{
     e.preventDefault()
-
+    message.textContent = ''
     let options = document.getElementsByName('option')
     let selected = []
     for(let i = 0; i < options.length; i++) {
         if(options[i].checked) {
             selected.push(options[i].value)
         }
+    }
+
+    if(selected.length===0){
+        return message.textContent = 'Please select an option'
     }
 
     console.log(selected)
@@ -54,16 +58,16 @@ voteForm.addEventListener('submit',(e)=>{
 
 const checkboxes = (data)=>{
     for(let i = 0; i < data.options.length; i++) {
-        $('#options').append('<input type="checkbox" id=option'+(i+1)+' name="option" value="'+(i+1)+'">')
-        .append('<label for="option'+(i+1)+'">'+data.options[i]+' - '+data.votes[i]+' votes</label><br>')
+        $('#options').append('<input type="checkbox" id=option'+(i)+' name="option" value="'+(i)+'">')
+        .append('<label for="option'+(i)+'">'+data.options[i]+' - '+data.votes[i]+' votes</label><br>')
     }
 }
 
 
 const radioButtons = (data)=>{
     for(let i = 0; i < data.options.length; i++) {
-        $('#options').append('<input type="radio" id=option'+(i+1)+' name="option" value="'+(i+1)+'">')
-        .append('<label for="option'+(i+1)+'">'+data.options[i]+' - '+data.votes[i]+' votes</label><br>')
+        $('#options').append('<input type="radio" id=option'+(i)+' name="option" value="'+(i)+'">')
+        .append('<label for="option'+(i)+'">'+data.options[i]+' - '+data.votes[i]+' votes</label><br>')
     }
 }
 
