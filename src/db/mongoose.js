3de889edbac5855118c6c96442631174e9c8a3be
Joe Chung <s1100841@monmouth.edu>
2020-03-07 17:04:24 -0500

    auto increment id and using mongoose schema

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,35 +1,48 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    autoIncrement = require('mongoose-auto-increment')
 
-mongoose.connect('mongodb://127.0.0.1:27017/anon-vote-api',{
+const connection = mongoose.createConnection('mongodb://127.0.0.1:27017/anon-vote-api',{
     useNewUrlParser:true,
     useCreateIndex:true,
-    useUnifiedTopology:true
+    useUnifiedTopology:true,
+    useFindAndModify:false
 })
 
-const Poll = mongoose.model('Poll',{
-    title:{
-        type:String
-    },
-    options:{
-        type:[String]
-    },
-    votes:{
-        type:[Number]
-    },
-    multi:{
-        type:Boolean
-    }
+autoIncrement.initialize(connection)
+
+const pollSchema = new Schema({
+    title:String,
+    options:[String],
+    votes:[Number],
+    multi:Boolean
 })
+
+pollSchema.plugin(autoIncrement.plugin,{model:'Poll',field:'id'})
+const Poll = connection.model('Poll',pollSchema)
 
 const poll1 = new Poll({
-    title:'Do you like me?',
+    title:'Yes or no?',
     options:['Yes','No'],
+    votes:[0,0],
+    multi:false
+})
+
+const poll2 = new Poll({
+    title:'Hot or cold?',
+    options:['Hot','Cold'],
     votes:[0,0],
     multi:false
 })
 
 poll1.save().then(()=>{
     console.log(poll1)
+}).catch((error)=>{
+    console.log('Error',error)
+})
+
+poll2.save().then(()=>{
+    console.log(poll2)
 }).catch((error)=>{
     console.log('Error',error)
 })
