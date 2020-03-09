e59df089e3884afd2675b8c5e8ebeaa946bae9cb
Joe Chung <s1100841@monmouth.edu>
2020-03-09 18:40:08 -0400

    create poll

--- public/js/createPoll.js
+++ public/js/createPoll.js
@@ -1,6 +1,40 @@
-var options = $('#options')
 var addOptionButton = document.getElementById('addOption')
 var removeOptionButton = document.getElementById('removeOption')
+var createForm = document.querySelector('form')
+var message = document.getElementById('message')
+
+createForm.addEventListener('submit',(e)=>{
+    e.preventDefault()
+
+    var formData
+    var optionObjects = []
+
+    var title = document.getElementById('pollTitle').value
+    var optionInputs = document.getElementsByClassName('option')
+
+    for(var i = 0; i <  optionInputs.length; i++){
+        var obj = {
+            "option": optionInputs[i].value,
+            "votes":0
+        }
+        optionObjects.push(obj)
+    }
+
+    formData = {
+        "title":title,
+        "options":optionObjects
+    }
+
+    fetch(window.location.href,{
+        method:'POST',
+        headers:{'Content-Type':'application/json'},
+        body: JSON.stringify(formData)
+    }).then((response)=>{        
+        response.json().then((data)=>{
+            window.location.href = '../polls/' + data.id
+        })
+    })
+})
 
 removeOptionButton.addEventListener('click',()=>{
     var optionCount = getOptionCount()
@@ -24,7 +58,7 @@ addOptionButton.addEventListener('click',()=>{
     html+='<label for="option-'+optionCount+'">Option ' + optionCount + '</label>'
     html+='<input type="text" class="option" name="option-'+optionCount+'">'
     html+='</div>'
-    options.append(html)
+    $('#options').append(html)
 })
 
 var getOptionCount = () =>{
