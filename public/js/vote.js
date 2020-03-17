50f5addb0b78b37be28ea1c1be722ce259f3496e
Joseph Chung <s1100841@monmouth.edu>
2020-03-17 18:26:15 -0400

    making url text input read only

--- public/js/vote.js
+++ public/js/vote.js
@@ -39,8 +39,6 @@ voteForm.addEventListener('submit',(e)=>{
 })
 
 copyUrl.addEventListener('click',()=>{
-    urlText.value=window.location.href
-
     urlText.select()
     urlText.setSelectionRange(0,99999)
 
