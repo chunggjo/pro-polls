109c3b79de3ffa935015d4cad9b4e40766fc5a8c
Christopher Calva <christopher.calva@gmail.com>
2020-03-17 15:18:37 -0400

    merge conlfict

--- src/server.js
+++ src/server.js
@@ -9,123 +9,135 @@ const ip = require('ip')
 
 const port = process.env.PORT || 3000
 
-const publicDirectoryPath = path.join(__dirname,'../public')
+const publicDirectoryPath = path.join(__dirname, '../public')
 
 const hbs = expbs.create({
-    defaultLayout:'main',
-    helpers:{
-        createPollForm: function(value){
-            var out = ''
-            for(var i = 0; i < value.length; i++){
-                var option = value[i].option
-                var votes = value[i].votes
-                out+='<div>'
-                out+='<input type="radio" name="option" value="'+option+'">'
-                out+='<label for="'+option+'">'+option+' - '+votes+' votes</label>'
-                out+='</div>'
-            }
-            return out
-        }
-    }
+	defaultLayout: 'main',
+	helpers: {
+		createPollForm: function(value) {
+			var out = ''
+			for (var i = 0; i < value.length; i++) {
+				var option = value[i].option
+				var votes = value[i].votes
+				out += '<div>'
+				out += '<input type="radio" name="option" value="' + option + '">'
+				out +=
+					'<label for="' +
+					option +
+					'">' +
+					option +
+					' - ' +
+					votes +
+					' votes</label>'
+				out += '</div>'
+			}
+			return out
+		}
+	}
 })
-app.engine('handlebars',hbs.engine)
-app.set('view engine','handlebars')
+app.engine('handlebars', hbs.engine)
+app.set('view engine', 'handlebars')
 
 app.use(express.static(publicDirectoryPath))
 app.use(express.json())
 
 app.use(requestIp.mw())
 
-app.get('/',(req,res)=>{
-    res.render('index',{
-        pageTitle:'AnonVote - Home',
-        headerText:'Welcome to AnonVote!'
-    })
+app.get('/', (req, res) => {
+	res.render('index', {
+		pageTitle: 'AnonVote - Home',
+		headerText: 'Welcome to AnonVote!'
+	})
 })
 
-app.get('/create',(req,res)=>{
-    res.render('create',{
-        pageTitle:'AnonVote - Create',
-        headerText:'Create a Poll'
-    })
+app.get('/create', (req, res) => {
+	res.render('create', {
+		pageTitle: 'AnonVote - Create',
+		headerText: 'Create a Poll'
+	})
 })
 
-app.post('/create',async(req,res)=>{
-    const poll = new Poll(req.body)
+app.post('/create', async (req, res) => {
+	const poll = new Poll(req.body)
 
-    poll.save().then(()=>{
-        res.status(201).send({
-            title:poll.title,
-            options:poll.options,
-            id:poll.id
-        })
-    }).catch((e)=>{
-        res.status(400).send(e)
-    })
+	poll
+		.save()
+		.then(() => {
+			res.status(201).send({
+				title: poll.title,
+				options: poll.options,
+				id: poll.id
+			})
+		})
+		.catch(e => {
+			res.status(400).send(e)
+		})
 })
 
-app.get('/polls/:id',async(req,res)=>{
-    try {
-        const poll = await Poll.findOne({id:req.params.id})
+app.get('/polls/:id', async (req, res) => {
+	try {
+		const poll = await Poll.findOne({ id: req.params.id })
 
-        if(!poll){
-            return res.status(404).render('404',{
-                pageTitle:'AnonVote - Poll not found',
-                headerText:'404 - Poll not found'
-            })
-        }
+		if (!poll) {
+			return res.status(404).render('404', {
+				pageTitle: 'AnonVote - Poll not found',
+				headerText: '404 - Poll not found'
+			})
+		}
 
-        res.render('poll',{
-            pageTitle:'AnonVote - Vote',
-            headerText:'Vote!',
-            pollTitle:poll.title,
-            options:poll.options
-        })
-    }catch(e){
-        res.status(500).send()
-    }
+		res.render('poll', {
+			pageTitle: 'AnonVote - Vote',
+			headerText: 'Vote!',
+			pollTitle: poll.title,
+			options: poll.options
+		})
+	} catch (e) {
+		res.status(500).send()
+	}
 })
 
-app.patch('/polls/:id',async(req,res)=>{
-    try {
-        const poll = await Poll.findOne({id: req.params.id})
-        
-        if(!poll){
-            return res.status(404).send()
-        }
-    
-        // Check ip for possible duplicate vote
-        const clientIp = ip.toBuffer(req.clientIp)
-        // const existingIp = poll.voters.find(o=>ip.toString(o.ip_buffer) === ip.toString(clientIp)
-        const existingIpIndex = poll.voters.map(x=>ip.toString(x.ip_buffer)).indexOf(ip.toString(clientIp))
-        if(existingIpIndex !== -1) {
-            return res.status(400).send()
-        }
+app.patch('/polls/:id', async (req, res) => {
+	try {
+		const poll = await Poll.findOne({ id: req.params.id })
 
-        poll.voters.push({'ip_buffer':clientIp})
-        
-        const option = poll.options.find(o => o.option === req.body.option)
-        option.votes+=1
+		if (!poll) {
+			return res.status(404).send()
+		}
 
-        await poll.save()
+		// Check ip for possible duplicate vote
+		const clientIp = ip.toBuffer(req.clientIp)
+		// const existingIp = poll.voters.find(o=>ip.toString(o.ip_buffer) === ip.toString(clientIp)
+		const existingIpIndex = poll.voters
+			.map(x => ip.toString(x.ip_buffer))
+			.indexOf(ip.toString(clientIp))
+		if (existingIpIndex !== -1) {
+			return res.status(400).send()
+		}
 
-        res.status(200).send({
-            title:poll.title,
-            options:poll.options,
-            id:poll.id
-        })
-    } catch(e) {
-        res.status(400).send(e)
-    }
+		poll.voters.push({ ip_buffer: clientIp })
+
+		const option = poll.options.find(o => o.option === req.body.option)
+		option.votes += 1
+
+		await poll.save()
+
+		res.status(200).send({
+			title: poll.title,
+			options: poll.options,
+			id: poll.id
+		})
+	} catch (e) {
+		res.status(400).send(e)
+	}
 })
 
-app.get('*',(req,res)=>{
-    res.render('404',{
-        pageTitle:'AnonVote - Page not found',
-        headerText:'404 - Page not found'
-    })
+app.get('*', (req, res) => {
+	res.render('404', {
+		pageTitle: 'AnonVote - Page not found',
+		headerText: '404 - Page not found'
+	})
 })
 
-app.listen(port, ()=>{
-    console.log('Server is up on port ' + port)
+app.listen(port, () => {
+	console.log('Server is up on port ' + port)
 })
