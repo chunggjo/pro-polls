983aa433e9458ce346e588d1fb3c4b7275082be3
Joseph Chung <s1100841@monmouth.edu>
2020-04-08 13:35:29 -0400

    preventing poll lookup by string

--- public/js/common/goToPoll.js
+++ public/js/common/goToPoll.js
@@ -1,20 +1,25 @@
-window.addEventListener( "pageshow", function ( event ) {
-    var historyTraversal = event.persisted || 
-                           ( typeof window.performance != "undefined" && 
-                                window.performance.navigation.type === 2 );
-    if ( historyTraversal ) {
-      // Handle page restore.
-      window.location.reload();
-    }
-});
+window.addEventListener('pageshow', function (event) {
+  var historyTraversal =
+    event.persisted ||
+    (typeof window.performance != 'undefined' &&
+      window.performance.navigation.type === 2)
+  if (historyTraversal) {
+    // Handle page restore.
+    window.location.reload()
+  }
+})
 const pollForm = document.querySelector('form')
 
 const search = document.querySelector('input')
 
-pollForm.addEventListener('submit',(e)=>{
-    e.preventDefault()
+pollForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  document.querySelector('#message').textContent = ''
 
-    const id = search.value
-
-    window.location.href = '/polls/'+id
+  const id = search.value
+  if (isNaN(id)) {
+    document.querySelector('#message').textContent = 'Please input a number.'
+  } else {
+    window.location.href = '/polls/' + id
+  }
 })
